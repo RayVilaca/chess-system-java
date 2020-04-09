4b6648f3ce15db0e8396ad34dc43e74c5592c4f5
Rayssa Vilaca <cecilia_vilaca@outlook.com>
2020-04-08 23:59:16 -0300

    BoardException and defensive programming

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ import chess.ChessMatch;
 
 public class Program {
 
-	public static void main(String[] args) {		// TODO Auto-generated method stub
+	public static void main(String[] args) {
 		ChessMatch chessMatch = new ChessMatch();
 		UI.printBoard(chessMatch.getPieces());
 	}
